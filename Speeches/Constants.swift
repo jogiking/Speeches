555bffa239430c670af2c9b383a0990e9eb21e6c
jogiking <whrldhks38@gmail.com>
2022-03-26 00:25:39 +0900

    🎨 HomeScene에서 사용하는 enum 추가

---
+++ Speeches/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Speeches
+//
+//  Created by turu on 2022/03/25.
+//
+
+import UIKit
+
+enum HomeScene {
+    static let maxHeight = UIScreen.main.bounds.height * 0.3
+    static let minHeight = UIScreen.main.bounds.height * 0.2
+}
