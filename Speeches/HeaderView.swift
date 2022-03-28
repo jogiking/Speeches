56fcd47a54f5aa3384e5bffd211d6f70b09e9981
jogiking <whrldhks38@gmail.com>
2022-03-29 03:31:55 +0900

    💄 headerView 대략적인 레이아웃 설정 추가
    펼쳤을 때 세로간격 조정해야함

--- Speeches/HeaderView.swift
+++ Speeches/HeaderView.swift
@@ -61,6 +61,14 @@ final class HeaderView: UIView {
         return button
     }()
     
+    private lazy var signInButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Sign in", for: .normal)
+        button.titleLabel?.font = .preferredFont(forTextStyle: .title3)
+        button.setTitleColor(UIColor.systemBlue, for: .normal)
+        return button
+    }()
+    
     private let inset = UIScreen.main.bounds.width * 0.08
 
     private func configureUI() {
@@ -68,34 +76,46 @@ final class HeaderView: UIView {
         
         addSubview(createAccountButton)
         createAccountButton.snp.makeConstraints { make in
-            make.bottom.equalToSuperview().inset(inset)
             make.trailing.equalToSuperview().inset(inset)
             make.height.equalTo(44)
-            make.width.equalTo(self.snp.height).priority(600)
-            make.leading.greaterThanOrEqualTo(inset).priority(800)
+            make.width.equalTo(self.snp.height).priority(500)
+            make.leading.greaterThanOrEqualTo(inset).priority(750)
+        }
+        
+        addSubview(signInButton)
+        signInButton.backgroundColor = .orange
+        signInButton.snp.makeConstraints { make in
+            make.centerX.equalTo(createAccountButton.snp.centerX)
+            make.top.equalTo(createAccountButton.snp.bottom).offset(8)
+            make.bottom.equalToSuperview().inset(8)
+            make.height.equalTo(self.snp.height).multipliedBy(16/HomeScene.maxHeight)
         }
         
         addSubview(imageView)
         imageView.snp.makeConstraints { make in
             make.size.equalTo(self.snp.width).multipliedBy(0.3)
             make.centerX.equalToSuperview()
-            make.bottom.equalToSuperview().inset(HomeScene.maxHeight * 0.6)
+            make.bottom.equalToSuperview().inset(HomeScene.maxHeight - HomeScene.minHeight * 0.25)
         }
         
         addSubview(headline)
         headline.backgroundColor = .red
         headline.snp.contentCompressionResistanceHorizontalPriority = 1000
         headline.snp.makeConstraints { make in
-            make.top.equalTo(self.snp.centerY).multipliedBy(1.2).priority(800)
+            make.top.equalTo(self.snp.centerY).multipliedBy(1.2).priority(500)
             make.top.greaterThanOrEqualTo(imageView.snp.bottom)
+            
+            make.top.lessThanOrEqualTo(createAccountButton.snp.top).priority(750)
+            make.bottom.lessThanOrEqualTo(createAccountButton.snp.bottom)
+            
             make.leading.equalToSuperview().offset(inset)
-            make.width.equalTo(self.snp.height).multipliedBy(0.5).priority(600)
+            make.width.equalTo(self.snp.height).multipliedBy(0.5).priority(500)
             make.trailing.lessThanOrEqualToSuperview().inset(UIScreen.main.bounds.width * 0.5)
         }
         
         addSubview(headline2)
         headline2.backgroundColor = .purple
-        headline2.snp.contentCompressionResistanceHorizontalPriority = 900
+        headline2.snp.contentCompressionResistanceHorizontalPriority = 750
         headline2.snp.makeConstraints { make in
             make.centerY.equalTo(headline)
             make.leading.equalTo(headline.snp.trailing)
@@ -106,10 +126,10 @@ final class HeaderView: UIView {
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        print(#function, frame.height, safeAreaInsets.top, frame.width, HomeScene.maxHeight, HomeScene.minHeight)
-        
-        imageView.alpha = (frame.height - HomeScene.minHeight)/HomeScene.maxHeight
-//        headline2.alpha = (frame.height - HomeScene.minHeight)/HomeScene.maxHeight
+        let alpha = (frame.height - HomeScene.minHeight)/HomeScene.maxHeight
+        imageView.alpha = alpha
+        headline2.alpha = alpha
+        signInButton.alpha = alpha
     }
 }
 
