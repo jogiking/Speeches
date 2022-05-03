86c36d3f871aca5fab5c2421d7bdc8a172212572
jogiking <whrldhks38@gmail.com>
2022-05-03 09:28:31 +0900

    ✨ Readable 객체를 위한 Respository 추가

---
+++ Speeches/Data/ReadableRepository.swift
@@ -0,0 +1,59 @@
+//
+//  ReadableRepository.swift
+//  Speeches
+//
+//  Created by turu on 2022/05/03.
+//
+
+import Foundation
+
+import RxSwift
+
+enum ReadableRepositoryError: Error {
+    case fetchFailed
+    case saveFailed
+    case deleteFailed
+}
+
+class ReadableRepository {
+    
+    func fetch() -> Single<[ReadableEntity]> {
+        return Single.create { single in
+            do {
+                let readableEntityObject = try RealmManager.shared.read(ReadableEntityObject.self)
+                let readableEntities = readableEntityObject.map {
+                    ReadableEntity(managedObject: $0)
+                }
+                single(.success(readableEntities))
+            } catch {
+                single(.failure(ReadableRepositoryError.fetchFailed))
+            }
+            return Disposables.create()
+        }
+        
+    }
+    
+    func save(_ entity: ReadableEntity) -> Single<Bool> {
+        return Single.create { single in
+            do {
+                try RealmManager.shared.create(entity.managedObject())
+            } catch {
+                single(.failure(ReadableRepositoryError.saveFailed))
+            }
+            single(.success(true))
+            return Disposables.create()
+        }
+    }
+
+    func delete(_ entity: ReadableEntity) -> Single<Bool> {
+        return Single.create { single in
+            do {
+                try RealmManager.shared.delete(entity.managedObject())
+            } catch {
+                single(.failure(ReadableRepositoryError.deleteFailed))
+            }
+            single(.success(true))
+            return Disposables.create()
+        }
+    }
+}
