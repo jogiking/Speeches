914dc263b555b0a076704ff1d913a61269cae667
jogiking <whrldhks38@gmail.com>
2022-05-04 00:22:35 +0900

    💄 AddURLViewController 추가

---
+++ Speeches/HomeScene/ViewControllers/AddURLViewController.swift
@@ -0,0 +1,67 @@
+//
+//  AddURLViewController.swift
+//  Speeches
+//
+//  Created by turu on 2022/05/04.
+//
+
+import UIKit
+
+import RxSwift
+import SnapKit
+
+class AddURLViewController: UIViewController {
+    
+    private var disposeBag = DisposeBag()
+    
+    private lazy var navigationBar: UINavigationBar = {
+        let navigationBar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 44))
+        let navigationItem = UINavigationItem(title: "Paste URL")
+        let rightItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(addBarButtonTouched(_:)))
+        
+        navigationItem.rightBarButtonItem = rightItem
+        navigationBar.items = [navigationItem]
+        return navigationBar
+    }()
+    
+    private lazy var urlInputField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Input Web URL"
+        return textField
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        configureUI()
+    }
+    
+    private func configureUI() {
+        view.addSubview(navigationBar)
+        navigationBar.snp.makeConstraints { make in
+            make.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        view.addSubview(urlInputField)
+        urlInputField.snp.makeConstraints { make in
+            make.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.top.equalTo(navigationBar.snp.bottom).offset(12)
+        }
+    }
+    
+    @objc private func addBarButtonTouched(_ sender: UIBarButtonItem) {
+        guard let documentTitle = urlInputField.text,
+              !documentTitle.isEmpty
+        else {
+            return
+        }
+    }
+    
+    private func presentAlert(_ description: String) {
+        let alert = UIAlertController(title: description, message: nil, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "OK", style: .cancel)
+        alert.addAction(alertAction)
+        present(alert, animated: true)
+    }
+}
