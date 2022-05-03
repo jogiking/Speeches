6174a0b4ff64c7740145bfb7d2f81b9bbb296e7d
jogiking <whrldhks38@gmail.com>
2022-05-04 00:22:07 +0900

    💄 AddTextViewController 추가
    plain text 추가하는 화면

--- Speeches/HomeScene/ViewControllers/AddTextViewController.swift
+++ Speeches/HomeScene/ViewControllers/AddTextViewController.swift
@@ -7,27 +7,86 @@
 
 import UIKit
 
+import RxSwift
 import SnapKit
 
 class AddTextViewController: UIViewController {
     
-    // TODO: Navigation Bar 붙이기
+    private var disposeBag = DisposeBag()
+    
+    private lazy var navigationBar: UINavigationBar = {
+        let navigationBar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 44))
+        let navigationItem = UINavigationItem(title: "Paste Text")
+        let rightItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(addBarButtonTouched(_:)))
+        
+        navigationItem.rightBarButtonItem = rightItem
+        navigationBar.items = [navigationItem]
+        return navigationBar
+    }()
+    
+    private lazy var documentInputField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Document Title"
+        return textField
+    }()
+    
+    private lazy var contentsInputField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Input Your Contents"
+        return textField
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        // Do any additional setup after loading the view.
+        
+        configureUI()
     }
     
+    private func configureUI() {
+        view.addSubview(navigationBar)
+        navigationBar.snp.makeConstraints { make in
+            make.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        view.addSubview(documentInputField)
+        documentInputField.snp.makeConstraints { make in
+            make.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.top.equalTo(navigationBar.snp.bottom).offset(12)
+        }
+        
+        view.addSubview(contentsInputField)
+        contentsInputField.snp.makeConstraints { make in
+            make.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.top.equalTo(documentInputField.snp.bottom).offset(8)
+        }
+    }
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
+    @objc private func addBarButtonTouched(_ sender: UIBarButtonItem) {
+        guard let documentTitle = documentInputField.text,
+              let contents = contentsInputField.text,
+              !documentTitle.isEmpty && !contents.isEmpty
+        else {
+            return
+        }
+        
+        let entity = ReadableEntity(date: Date().currentDateString, imageURL: [String](), title: documentTitle, contents: contents)
+        
+        ReadableRepository().save(entity).subscribe { event in
+            switch event {
+            case .success(let flag):
+                // TODO: Home 화면으로 돌아가기
+                print(#function, #line, flag)
+            case .failure(let error):
+                self.presentAlert(error.localizedDescription)
+            }
+        }.disposed(by: disposeBag)
+    }
     
+    private func presentAlert(_ description: String) {
+        let alert = UIAlertController(title: description, message: nil, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "OK", style: .cancel)
+        alert.addAction(alertAction)
+        present(alert, animated: true)
+    }
 }
