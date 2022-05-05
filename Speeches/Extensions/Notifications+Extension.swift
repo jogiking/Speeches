38eb34ac6d63f5a9c6f70ddd39f03fa7d90cd540
jogiking <whrldhks38@gmail.com>
2022-05-05 15:54:57 +0900

    ✨ NotificationCenter Extension추가

---
+++ Speeches/Extensions/Notifications+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  Notifications+Extension.swift
+//  Speeches
+//
+//  Created by turu on 2022/05/05.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let readableRepositoryChanged = Notification.Name("readableRepositoryChanged")
+}
